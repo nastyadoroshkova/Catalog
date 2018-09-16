b6bef1ca3e369e7e3323e67ae31e083fc5f82534
nastyadoroshkova <doroshkova.nastya@gmail.com>
2018-09-16 18:56:16 +0300

    added REDUX_DEVTOOLS_EXTENSION

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,8 @@ import rootReducer from '../reducers';
 const createAppStore = () => {
     const store = createStore(
         rootReducer,
-        {}
+        window.__REDUX_DEVTOOLS_EXTENSION__&&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
     ); 
 
     return store;
